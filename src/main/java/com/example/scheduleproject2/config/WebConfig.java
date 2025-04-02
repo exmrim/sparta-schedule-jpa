0a86fcbae4c464e4c659c41fe2a2940f3c787967
exmrim <exmrim@gmail.com>
2025-04-02 11:43:09 +0900

    add WebConfig

--- src/main/java/com/example/scheduleproject2/config/WebConfig.java
+++ src/main/java/com/example/scheduleproject2/config/WebConfig.java
@@ -1,4 +1,23 @@
 package com.example.scheduleproject2.config;
 
-public class WebConfig {
+import com.example.scheduleproject2.filter.loginFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public FilterRegistrationBean loginFilter() {
+
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+        filterRegistrationBean.setFilter(new loginFilter());
+        filterRegistrationBean.setOrder(1);
+        filterRegistrationBean.addUrlPatterns("/*");
+
+        return filterRegistrationBean;
+
+    }
 }
